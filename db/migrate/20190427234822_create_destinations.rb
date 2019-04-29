59bb7b5228fdd8f093b3daf5e09bccb7e6613c4a
Lali Gabelaya <laligabelaya@gmail.com>
2019-04-29 01:18:05 -0700

    migrations

--- db/migrate/20190427234822_create_destinations.rb
+++ db/migrate/20190427234822_create_destinations.rb
@@ -1,6 +1,10 @@
 class CreateDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :destinations do |t|
+      t.string :name
+      t.string :image
+      t.string :description
+      t.references :tag 
 
       t.timestamps
     end

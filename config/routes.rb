5c3371531cf6e92f7729f9d417bdb9a60cfec0c1
Lali Gabelaya <laligabelaya@gmail.com>
2019-04-29 01:57:44 -0700

    show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tags', to: 'tags#index' # homepage
+  get '/tags/:id' => 'tags#show', as: :tag 
 end
